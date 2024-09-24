9647cd37ea8f03a64c59593088c3c99b0c3d0825
farihaa1 <farihaa1403@gmail.com>
2024-09-24 08:38:17 +0600

    done home page and added blogpage

--- app.js
+++ app.js
@@ -1,48 +1,51 @@
 document.addEventListener("DOMContentLoaded",
-    function(event){
-    
-    event.preventDefault();
-        
-    function toggleSections(showSection, hideSection) {
-        const active = document.getElementById(showSection);
-        const deActive = document.getElementById(hideSection);
-
-        active.classList.remove('hidden');
-        deActive.classList.add('hidden');
-
-
-
-    }
-
-    function changeButtonStyles(activeButtonId, inactiveButtonId) {
-        const activeButton = document.getElementById(activeButtonId);
-        const inactiveButton = document.getElementById(inactiveButtonId);
-
-        activeButton.classList.add('bg-primaryBg', 'text-black'); // Active button styles
-        activeButton.classList.remove('bg-white', 'text-black'); // Remove inactive styles
-
-        inactiveButton.classList.add('bg-white', 'text-black'); // Inactive button styles
-        inactiveButton.classList.remove('bg-primaryBg', 'text-black'); // Remove active styles
-    }
-
-    // donation btn
-    document.getElementById("donationBtn").addEventListener("click", function (event) {
+    function (event) {
 
         event.preventDefault();
-        toggleSections('donation', 'history');
-        changeButtonStyles("donationBtn", "historyBtn")
-        console.log("clicked")
-    });
 
-    // History btn
-    document.getElementById("historyBtn").addEventListener("click", function (event) {
-        event.preventDefault();
-        toggleSections('history', 'donation');
-        changeButtonStyles( "historyBtn", "donationBtn")
-        console.log("history clicked")
-    });
+        function toggleSections(showSection, hideSection) {
+            const active = document.getElementById(showSection);
+            const deActive = document.getElementById(hideSection);
 
-    // Donation logic
+            active.classList.remove('hidden');
+            deActive.classList.add('hidden');
+
+
+
+        }
+
+
+
+
+        function changeButtonStyles(activeButtonId, inactiveButtonId) {
+            const activeButton = document.getElementById(activeButtonId);
+            const inactiveButton = document.getElementById(inactiveButtonId);
+
+            activeButton.classList.add('bg-primaryBg'); // Active button styles
+            activeButton.classList.remove('bg-white'); // Remove inactive styles
+
+            inactiveButton.classList.add('bg-white'); // Inactive button styles
+            inactiveButton.classList.remove('bg-primaryBg'); // Remove active styles
+        }
+
+        // donation btn
+        document.getElementById("donationBtn").addEventListener("click", function (event) {
+
+            event.preventDefault();
+            toggleSections('donation', 'history');
+            changeButtonStyles("donationBtn", "historyBtn");
+
+        });
+
+        // History btn
+        document.getElementById("historyBtn").addEventListener("click", function (event) {
+            event.preventDefault();
+            toggleSections('history', 'donation');
+            changeButtonStyles("historyBtn", "donationBtn")
+
+        });
+
+        // Donation logic
 
         function handleDonation(donationInputId, usersTotalId, targetTotalId) {
             const usersTotalIk = document.getElementById(usersTotalId);
@@ -72,26 +75,117 @@ document.addEventListener("DOMContentLoaded",
 
         }
 
-        document.getElementById('noakhaliDonate').addEventListener('click', function(event){
-            event.preventDefault();
+        // history
+
+
+
+        function addElement(donateContent, donationInputValue) {
+
+            const donationTk = parseFloat(donationInputValue);
+
+            // validation
+            if (isNaN(donationTk) || donationTk <= 0) {
+                alert("Invalid donation amount");
+                return;
+            }
+
+            // get the current date
+            const currentDate = new Date();
+
+
+
+            // create a new div element
+            const newDiv = document.createElement("div");
+
+
+            // and give it some content
+
+            newDiv.classList.add("p-5", "m-2", "border", "rounded-xl","w-full", "gap-3");
+
+            // add the content sections
+            const newContent = `
             
-            handleDonation('donate-noakhali', 'usersTotalTk', 'noakhali')
-        });
-        document.getElementById('donateFeni').addEventListener('click', function(event){
-            event.preventDefault();
-            handleDonation('donate-feni', 'usersTotalTk', 'feni')
+            <h3 class = "text-xl font-semibold m-2">${donationTk} BDT donated ${donateContent}</h3>
+            <p class = " m-2 text-sm text-gray-500">Date: ${currentDate}</p>
+            
+            `;
+
+            newDiv.innerHTML = newContent;
+
+            // Append the new div to the history section
+
+            const historySection = document.getElementById("div1");
+            if (!historySection) {
+                console.error("No history section found");
+                return;
+            }
+            historySection.classList.remove('hidden');
+            historySection.appendChild(newDiv);
+        }
+
+
+
+
+        // popup
+        function popSection() {
+            document.getElementById('popUp').classList.remove('hidden');
+        }
+
+        document.getElementById('close').addEventListener('click', function () {
+            document.getElementById('popUp').classList.add('hidden');
         });
 
-        document.getElementById('donateQuota').addEventListener('click', function(event){
+
+
+
+
+
+
+
+        document.getElementById('noakhaliDonate').addEventListener('click', function (event) {
             event.preventDefault();
-            handleDonation('donate-quota', 'usersTotalTk', 'quota')
+            const donationInputValue = document.getElementById('donate-noakhali').value;
+            const donateContentFlood = "for flood relief at Noakhali, Bangladesh";
+
+            handleDonation('donate-noakhali', 'usersTotalTk', 'noakhali');
+
+            popSection();
+            addElement(donateContentFlood, donationInputValue)
+
+
+
+
+
+        });
+        document.getElementById('donateFeni').addEventListener('click', function (event) {
+            event.preventDefault();
+            const donationInputValue = document.getElementById('donate-feni').value;
+            const donateContentFamine = "for famine-2024 at Feni, Bangladesh";
+            handleDonation('donate-feni', 'usersTotalTk', 'feni');
+
+            popSection();
+            addElement(donateContentFamine, donationInputValue);
+
+
+        });
+
+        document.getElementById('donateQuota').addEventListener('click', function (event) {
+            event.preventDefault();
+
+            handleDonation('donate-quota', 'usersTotalTk', 'quota');
+
+
+            popSection();
+            const donationInputValue = document.getElementById('donate-quota').value;
+            const donateContentQuota = "aid for the Injured in quota Movement, Bangladesh";
+            addElement(donateContentQuota, donationInputValue);
         });
 
 
         // history elements
 
 
-        
+
     });
 
 
